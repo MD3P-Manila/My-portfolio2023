f403314759a138fc1caa75269a98a1fadb1dfd9e
BimoBaby <manilarayandrew@gmail.com>
2023-05-09 15:47:40 +0800

    Resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -109,7 +109,7 @@ const Navbar = () => {
 
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                 <a className='flex justify-between items-center w-full text-gray-300'
-                href="/">
+                href="https://drive.google.com/file/d/1_XZ2PYeits-5wAhYASxLBigJEJhgnBlm/view?usp=share_link">
                     Resume <BsFillPersonLinesFill size={30}/>
                 </a>
             </li>
