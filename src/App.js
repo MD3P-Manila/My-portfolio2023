c2f40b6ef229db97d85f4f591e4e82eedd3bb178
BimoBaby <manilarayandrew@gmail.com>
2023-05-30 01:39:09 +0800

    new resume

--- src/App.js
+++ src/App.js
@@ -4,38 +4,19 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Work from './components/Work';
-import React, { useState, useEffect } from 'react';
-import { useMediaQuery } from './index.css';
 
-const App = () => {
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    setIsMobile(window.matchMedia('(max-width: 320px)').matches && window.matchMedia('(max-height: 320px)').matches);
-  }, []);
-
+function App() {
   return (
     <div>
       <Navbar />
-      {isMobile ? (
-        <div>
-          <Home mobile />
-          <About mobile />
-          <Skills mobile />
-          <Work mobile />
-          <Contact mobile />
-        </div>
-      ) : (
-        <div>
-          <Home />
-          <About />
-          <Skills />
-          <Work />
-          <Contact />
-        </div>
-      )}
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
-};
+}
 
-export default App;
+export default App;  
