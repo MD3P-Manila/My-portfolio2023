cc9fb00d67a2c5a0ff1bd32a2757c72b988fa319
R.A Manila <md3pmanila@gmail.com>
2024-02-14 23:50:33 +0800

    Revert "about section update"
    This reverts commit e25274de04c2256cd8502f54d3fbb49527f3357c.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -45,7 +45,7 @@ const Skills = () => {
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                    <p className='my-4'>LARAVEL 10</p>
+                    <p className='my-4'>NODE JS</p>
                 </div>
                 
             </div>
